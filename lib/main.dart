4b19a8ad790c96a1a480551db523f5a57c4c5a32
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-07-25 15:32:57 -0700

    1.3: Extract custom LocationDetail stateless widget from main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,8 @@
 import 'package:flutter/material.dart';
+import 'location_detail.dart';
 
 void main() {
   return runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello, World!"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [Container(
-          decoration: BoxDecoration(
-            color: Colors.red,
-          ),
-        child: Text("One"),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.green,
-          ),
-        child: Text("Two"),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.blue,
-          ),
-        child: Text("Three"),
-        ),
-      ]),
-    ),
+    home: LocationDetail()
   ));
 }

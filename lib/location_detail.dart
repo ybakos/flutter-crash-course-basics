442f71f3bf625199f1fa7cad62813c8d1ad9dc6c
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-07-25 16:53:38 -0700

    1.4: Extract Column elements into function that generates a List.

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -16,22 +16,26 @@ class LocationDetail extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          _section("One", Colors.red),
-          _section("Two", Colors.green),
-          _section("Three", Colors.blue),
-        ]
+        children: _renderFacts(location)
       ),
     );
   }
 
-  Widget _section(String title, Color color) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color,
-      ),
-      child: Text(title),
-    );
+  List<Widget> _renderFacts(Location location) {
+    var result = List<Widget>();
+    for (int i = 0; i < location.facts.length; ++i) {
+      result.add(_sectionTitle(location.facts[i].title));
+      result.add(_sectionText(location.facts[i].text));
+    }
+    return result;
+  }
+
+  Widget _sectionTitle(String text) {
+    return Text(text);
+  }
+
+  Widget _sectionText(String text) {
+    return Text(text);
   }
 
 }

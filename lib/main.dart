e0fa021cfba7c67edcc93ac18d4ba08b81ecbc9f
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-07-25 14:57:46 -0700

    1.2: Use Column with multiple Containers as children.
    Using red, green and blue backgrounds for each Container.

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,26 @@ void main() {
       appBar: AppBar(
         title: Text("Hello, World!"),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.red,
-        ),
+      body: Column(
+        children: [Container(
+          decoration: BoxDecoration(
+            color: Colors.red,
+          ),
         child: Text("One"),
-      ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.green,
+          ),
+        child: Text("Two"),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+        child: Text("Three"),
+        ),
+      ]),
     ),
   ));
-} 
+}

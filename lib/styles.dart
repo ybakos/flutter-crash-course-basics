52296600b557912a7a67e406118b669371a514af
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-07-25 21:38:46 -0700

    1.6: Add font.

--- lib/styles.dart
+++ lib/styles.dart
@@ -6,15 +6,16 @@ class Styles {
   static const _textSizeDefault = 16.0;
   static final Color _textColorStrong = _hexToColor('000000');
   static final Color _textColorDefault = _hexToColor('666666');
-  
+  static final String _fontNameDefault = 'Muli';
+
   static final headerLarge = TextStyle(
-    //fontFamily: _fontNameDefault,
+    fontFamily: _fontNameDefault,
     fontSize: _textSizeLarge,
     color: _textColorStrong
   );
   
   static final textDefault = TextStyle(
-    //fontFamily: _fontNameDefault,
+    fontFamily: _fontNameDefault,
     fontSize: _textSizeDefault,
     color: _textColorDefault
   );

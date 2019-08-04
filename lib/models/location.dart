f68146e5701223017f5e44482f4096e42b26bcb3
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-08-04 14:33:27 -0700

    2.12: Location, add real fetchAll and fetchById, and test.

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:http/http.dart' as http;
 import 'location_fact.dart';
+import '../endpoint.dart';
 
 part 'location.g.dart';
 
@@ -14,6 +17,28 @@ class Location {
 
   factory Location.fromJson(Map<String, dynamic> json) {
     return _$LocationFromJson(json);
+  }
+
+  static Future<List<Location>> fetchAll() async {
+    var uri = Endpoint.uri('/locations');
+    final response = await http.get(uri.toString());
+    if (response.statusCode != 200) {
+      throw response.body;
+    }
+    List<Location> locations = <Location>[];
+    for (var jsonItem in json.decode(response.body)) {
+      locations.add(Location.fromJson(jsonItem));
+    }
+    return locations;
+  }
+
+  static Future<Location> fetchById(int id) async {
+    var uri = Endpoint.uri('/locations/$id');
+    final response = await http.get(uri.toString());
+    if (response.statusCode != 200) {
+      throw response.body;
+    }
+    return Location.fromJson(json.decode(response.body));
   }
 
 }

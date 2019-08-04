c8a7bb852162ebf807304288501cec01f04120ea
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-08-04 13:27:59 -0700

    2.12: Location, add id attribute.

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -5,11 +5,12 @@ part 'location.g.dart';
 
 @JsonSerializable()
 class Location {
+  final int id;
   final String name;
   final String url;
   final List<LocationFact> facts;
   
-  Location({this.name, this.url, this.facts});
+  Location({this.id, this.name, this.url, this.facts});
 
   factory Location.fromJson(Map<String, dynamic> json) {
     return _$LocationFromJson(json);
